60b7c7c01dd8bfdab3620475d0eb6602d2ccc018
syobonaction <smashdaisaku@gmail.com>
2015-07-28 23:29:30 -0500

    Error Fixes
    Everything is working correctly now aside from camera follow which is
    broken now. Will fix soon.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,13 +4,15 @@ using System.Collections;
 public class CameraFollow: MonoBehaviour {
 	
 	public float dampTime = 0.15f;
-	private Vector3 velocity = Vector3.zero;
+	private Vector3 velocity;
 	public Transform target;
 	
+	public Camera cam;
 
 	//called once when the script is run.
 	void Start () {
-
+		velocity = Vector3.zero;
+		cam = GetComponent<Camera>();
 	}
 	
 	// Update is called once per frame
@@ -18,8 +20,8 @@ public class CameraFollow: MonoBehaviour {
 	{
 		if (target)
 		{
-			Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
-			Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 point = cam.WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
 			Vector3 destination = transform.position + delta;
 			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
 		}
